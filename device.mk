8149a67f130c6576b938eb16dbce6616c1bd53e6
COSMIC <rayavanindra@gmail.com>
2024-09-21 15:49:05 +0000

    miatoll: Added MiuiCamera support [1/2]
    Co-authored-by: Arian <arian.kulmer@web.de>
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,25 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 USE_DEX2OAT_DEBUG := false
 
+# Camera (MiuiCamera)
+# Priv-app permission
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/miuicamera/privapp-permissions-miui.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miui.xml
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/miuicamera/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
+
+# Camera (shims for MiuiCamera)
+PRODUCT_PACKAGES += \
+    libgui_shim_miuicamera
+
+TARGET_USES_MIUI_CAMERA := true
+
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    MiuiCamera
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
