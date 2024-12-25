944720d953aeeff3565bfb0c30634de40e041aef
ritvikct <ritvikct@proton.me>
2024-12-25 12:41:06 +0000

    miatolll: exclued more packages

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Aperture Maps Drive PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt Browser arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 GrapheneCamera
+LOCAL_OVERRIDES_PACKAGES := Markup MusicFX Aperture Chrome Maps Drive Photos PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 Browser Gallery GrapheneCamera Sounds 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
