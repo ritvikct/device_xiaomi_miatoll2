0d1ffd764af64898c480d243ba1fc3e796d2e8ed
ritvikct <ritvikct@proton.me>
2024-12-26 02:30:42 +0000

    miatoll: remove packages

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Markup MusicFX Aperture Chrome Maps Drive Photos PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 Browser Gallery GrapheneCamera Sounds 
+LOCAL_OVERRIDES_PACKAGES := Markup MusicFX GoogleChrome Maps Drive Photos PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Browser Gallery GrapheneCamera Sounds org.fossify.gallery com.simpletools.musicplayer com.android.chrome com.android.soundpicker
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
